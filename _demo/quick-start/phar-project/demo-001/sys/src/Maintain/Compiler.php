fd419bf2be7558312f8139d5a356c3df0f5b8eb9
samwhelp <samwhelp@users.noreply.github.com>
2024-07-16 18:49:50 +0800

    fix

--- _demo/quick-start/phar-project/demo-001/sys/src/Maintain/Compiler.php
+++ _demo/quick-start/phar-project/demo-001/sys/src/Maintain/Compiler.php
@@ -3,6 +3,23 @@
 namespace Maintain;
 
 
+
+
+/*
+
+## Link
+
+* https://symfony.com/doc/current/components/filesystem.html
+
+
+*/
+
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
+
+
 class Compiler {
 
 
@@ -10,10 +27,19 @@ class Compiler {
 
 		var_dump(__FILE__);
 
-
 		$project_root_dir_path = $this->_ProjectRootDirPath;
 
-		var_dump($project_root_dir_path);
+		$filesystem = new Filesystem();
+
+		if (!file_exists("{$project_root_dir_path}/lib/vendor")) {
+			chdir($project_root_dir_path);
+			system('composer install');
+		}
+
+		if (file_exists("{$project_root_dir_path}/tmp")) {
+			echo "## Remove [tmp]({$project_root_dir_path}/tmp)";
+	 		$filesystem->remove("{$project_root_dir_path}/tmp");
+		}
 
 	}
 
