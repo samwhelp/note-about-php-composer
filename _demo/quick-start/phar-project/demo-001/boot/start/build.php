e06c2aa009a8e5cf518d867bc2bfea30920dc94b
samwhelp <samwhelp@users.noreply.github.com>
2024-07-16 21:31:30 +0800

    fix

--- _demo/quick-start/phar-project/demo-001/boot/start/build.php
+++ _demo/quick-start/phar-project/demo-001/boot/start/build.php
@@ -3,6 +3,6 @@
 	require_once(THE_PRJ_ROOT_DIR_PATH . '/boot/loader/main.php');
 
 	(new Maintain\Compiler())
-		//->setProjectRootDirPath(THE_PRJ_ROOT_DIR_PATH)
+		->setProjectRootDirPath(THE_PRJ_ROOT_DIR_PATH)
 		->setTargetPharFileName('demo.phar')
 		->run();
